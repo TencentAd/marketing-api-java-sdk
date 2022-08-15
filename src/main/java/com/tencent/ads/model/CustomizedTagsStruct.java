eb19ff58f22bd7e8d987ea070351a7425f19be56
tencent_ad_developer <tencent_ad_developer@tencent.com>
2022-08-15 15:01:31 +0800

    auto update

--- src/main/java/com/tencent/ads/model/CustomizedTagsStruct.java
+++ src/main/java/com/tencent/ads/model/CustomizedTagsStruct.java
@@ -13,12 +13,67 @@
 package com.tencent.ads.model;
 
 import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /** 自定义标签集合 */
 @ApiModel(description = "自定义标签集合")
 public class CustomizedTagsStruct {
+  @SerializedName("tag_group_name")
+  private String tagGroupName = null;
+
+  @SerializedName("tag_name_list")
+  private List<String> tagNameList = null;
+
+  public CustomizedTagsStruct tagGroupName(String tagGroupName) {
+    this.tagGroupName = tagGroupName;
+    return this;
+  }
+
+  /**
+   * Get tagGroupName
+   *
+   * @return tagGroupName
+   */
+  @ApiModelProperty(value = "")
+  public String getTagGroupName() {
+    return tagGroupName;
+  }
+
+  public void setTagGroupName(String tagGroupName) {
+    this.tagGroupName = tagGroupName;
+  }
+
+  public CustomizedTagsStruct tagNameList(List<String> tagNameList) {
+    this.tagNameList = tagNameList;
+    return this;
+  }
+
+  public CustomizedTagsStruct addTagNameListItem(String tagNameListItem) {
+    if (this.tagNameList == null) {
+      this.tagNameList = new ArrayList<String>();
+    }
+    this.tagNameList.add(tagNameListItem);
+    return this;
+  }
+
+  /**
+   * Get tagNameList
+   *
+   * @return tagNameList
+   */
+  @ApiModelProperty(value = "")
+  public List<String> getTagNameList() {
+    return tagNameList;
+  }
+
+  public void setTagNameList(List<String> tagNameList) {
+    this.tagNameList = tagNameList;
+  }
 
   @Override
   public boolean equals(java.lang.Object o) {
@@ -28,12 +83,14 @@ public class CustomizedTagsStruct {
     if (o == null || getClass() != o.getClass()) {
       return false;
     }
-    return true;
+    CustomizedTagsStruct customizedTagsStruct = (CustomizedTagsStruct) o;
+    return Objects.equals(this.tagGroupName, customizedTagsStruct.tagGroupName)
+        && Objects.equals(this.tagNameList, customizedTagsStruct.tagNameList);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash();
+    return Objects.hash(tagGroupName, tagNameList);
   }
 
   @Override
